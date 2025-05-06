d36bf02a005387a2f12ae2aa52b339460d1ecdda
rohit8708913 <koyeb67user@gmail.com>
2025-05-06 12:15:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -26,3 +26,28 @@ START_PIC = os.environ.get("START_PIC", "https://telegra.ph/file/ec17880d61180d3
 #--------------------------------------------
 #--------------------------------------------
 START_MSG = os.environ.get("START_MESSAGE", "<b>ʜᴇʟʟᴏ {first}\n\n<blockquote> ɪ ᴀᴍ ᴘᴏsᴛ ᴍᴀɴᴀɢᴇᴍᴇɴᴛ ʙᴏᴛ.</blockquote></b>")
+
+#--------------------------------------------
+#--------------------------------------------
+
+LOG_FILE_NAME = "filesharingbot.txt"
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    datefmt='%d-%b-%y %H:%M:%S',
+    handlers=[
+        RotatingFileHandler(
+            LOG_FILE_NAME,
+            maxBytes=50000000,
+            backupCount=10
+        ),
+        logging.StreamHandler()
+    ]
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+def LOGGER(name: str) -> logging.Logger:
+    return logging.getLogger(name)
+   
