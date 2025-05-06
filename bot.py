0b0c7c53ae6eeea7e33142bd5aeb0afc97751fd9
rohit8708913 <koyeb67user@gmail.com>
2025-05-06 12:50:07 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,27 +1,20 @@
-#rohit_1888
-
-
 from aiohttp import web
 import asyncio
 import pyromod.listen
-from pyrogram import Client
-from pyrogram.types import InputMediaPhoto
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
-from pyrogram.handlers import MessageHandler
-from pyrogram import filters
-from collections import defaultdict
-import asyncio
+from pyrogram import Client, filters
+from pyrogram.types import InputMediaPhoto, Message
 from pyrogram.enums import ParseMode
+from collections import defaultdict
+from datetime import datetime
+import logging
 import sys
 import pytz
-from datetime import datetime
-#rohit_1888 on Tg
+
+# Custom config and database imports
 from config import *
 from database import *
 
-import logging
-
-
+# Web route setup
 routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
@@ -33,7 +26,7 @@ async def web_server():
     web_app.add_routes(routes)
     return web_app
 
-
+# Bot Client
 class Bot(Client):
     def __init__(self):
         super().__init__(
@@ -50,29 +43,25 @@ class Bot(Client):
         usr_bot_me = await self.get_me()
         self.uptime = datetime.now()
 
-
-        self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/weebs_support")
-
         self.set_parse_mode(ParseMode.HTML)
         self.username = usr_bot_me.username
-        self.LOGGER(__name__).info(f"Bot Running..! Made by @Rohit_1888")   
+        self.LOGGER(__name__).info(f"Bot Running..! Made by @Rohit_1888")
 
-        # Start Web Server
+        # Start web server
         app = web.AppRunner(await web_server())
         await app.setup()
         await web.TCPSite(app, "0.0.0.0", PORT).start()
 
-
-        try: await self.send_message(OWNER_ID, text = f"<b><blockquote> Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ by @Codeflix_Bots</blockquote></b>")
-        except: pass
+        try:
+            await self.send_message(OWNER_ID, text="<b><blockquote> Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ by @Codeflix_Bots</blockquote></b>")
+        except:
+            pass
 
     async def stop(self, *args):
         await super().stop()
         self.LOGGER(__name__).info("Bot stopped.")
 
     def run(self):
-        """Run the bot."""
         loop = asyncio.get_event_loop()
         loop.run_until_complete(self.start())
         self.LOGGER(__name__).info("Bot is now running. Thanks to @rohit_1888")
@@ -83,12 +72,12 @@ class Bot(Client):
         finally:
             loop.run_until_complete(self.stop())
 
+# Create bot instance
+app = Bot()
 
-@Bot.on_message(filters.command('start') & filters.private)
+# Start command
+@app.on_message(filters.command('start') & filters.private)
 async def start_command(client: Client, message: Message):
-    user_id = message.from_user.id
-    id = message.from_user.id
-
     await message.reply_photo(
         photo=START_PIC,
         caption=START_MSG.format(
@@ -99,37 +88,34 @@ async def start_command(client: Client, message: Message):
             id=message.from_user.id
         ),
         reply_markup=reply_markup
-        # message_effect_id=5104841245755180586  
     )
 
-    return
-
-
-
+# Handle media group (album)
 media_groups = defaultdict(list)
 
-@Bot.on_message(filters.private & filters.media_group)
-async def handle_album(client, message):
+@app.on_message(filters.private & filters.media_group)
+async def handle_album(client: Client, message: Message):
     media_groups[message.media_group_id].append(message)
-
-    # Delay briefly to wait for all messages in the group
-    await asyncio.sleep(1.5)
+    await asyncio.sleep(1.5)  # allow time for all media in group
 
     messages = media_groups.pop(message.media_group_id, [])
     media = []
+    added_caption = False
 
     for msg in sorted(messages, key=lambda m: m.message_id):
-        caption = msg.caption or ""
-        new_caption = f"@Javpostr\n\n{caption}" if caption else "@Javpostr"
-        if len(media) == 0:
-            # Only first media in group can have caption
-            media.append(InputMediaPhoto(media=msg.photo.file_id, caption=new_caption))
+        if msg.caption:
+            caption = f"@Javpostr\n\n{msg.caption}"
+            if not added_caption:
+                media.append(InputMediaPhoto(media=msg.photo.file_id, caption=caption))
+                added_caption = True
+            else:
+                media.append(InputMediaPhoto(media=msg.photo.file_id))
         else:
             media.append(InputMediaPhoto(media=msg.photo.file_id))
 
     if media:
         await message.reply_media_group(media=media)
 
-
+# Start bot
 if __name__ == "__main__":
-    Bot().run()
+    app.run()
