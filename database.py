3d74180ff36c6df40eac2a98dd91a805f38b209f
rohit8708913 <koyeb67user@gmail.com>
2025-05-06 12:29:33 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,30 @@
 #rohit_1888
+
+
+import motor, asyncio
+import motor.motor_asyncio
+from config import DB_URI, DB_NAME
+
+dbclient = motor.motor_asyncio.AsyncIOMotorClient(DB_URI)
+database = dbclient[DB_NAME]
+
+user_data = database['users']
+
+
+async def present_user(user_id: int):
+    found = await user_data.find_one({'_id': user_id})
+    return bool(found)
+
+async def add_user(user_id: int):
+    user = new_user(user_id)
+    await user_data.insert_one(user)
+    return
+
+async def full_userbase():
+    user_docs = user_data.find()
+    user_ids = [doc['_id'] async for doc in user_docs]
+    return user_ids
+
+async def del_user(user_id: int):
+    await user_data.delete_one({'_id': user_id})
+    return
