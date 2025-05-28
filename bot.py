ab3631f9b21333432fff92109d59bee218d6810a
rohit3607 <rohitplayer87089@gmail.com>
2025-05-28 16:09:54 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -236,6 +236,36 @@ async def handle_download_button(client: Client, callback_query):
         if os.path.exists(pdf_path):
             os.remove(pdf_path)
 
+#-------------------------------------------#
+@app.on_message(filters.command("update") & filters.user(ADMINS))
+async def update_bot(client, message):
+    #if message.from_user.id not OWNER_ID:
+        #return await message.reply_text("You are not authorized to update the bot.")
+
+    try:
+        msg = await message.reply_text("<b><blockquote>Pulling the latest updates and restarting the bot...</blockquote></b>")
+
+        # Run git pull
+        git_pull = subprocess.run(["git", "pull"], capture_output=True, text=True)
+
+        if git_pull.returncode == 0:
+            await msg.edit_text(f"<b><blockquote>Updates pulled successfully:\n\n{git_pull.stdout}</blockquote></b>")
+        else:
+            await msg.edit_text(f"<b><blockquote>Failed to pull updates:\n\n{git_pull.stderr}</blockquote></b>")
+            return
+
+        await asyncio.sleep(3)
+
+        await msg.edit_text("<b><blockquote>✅ Bot is restarting now...</blockquote></b>")
+
+    except Exception as e:
+        await message.reply_text(f"An error occurred: {e}")
+        return
+
+    finally:
+        # Restart the bot process
+        os.execl(sys.executable, sys.executable, *sys.argv)
+
 # Start bot
 if __name__ == "__main__":
     app.run()
